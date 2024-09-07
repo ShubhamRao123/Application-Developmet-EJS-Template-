51e25898fd6fafc2e0c83b206764d36b7ae02d01
Shubham Rao <shubhamrao834@gmail.com>
2024-09-08 02:07:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let siteName = "Adidas";
+  let searchText = "Search Now";
+  res.render("index", { siteName: siteName, searchText: searchText });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
